1fa9ec3b4a4427afa01484128cadda189fa898f8
Emilie CLARY <emilie.clary@student.yncrea.fr>
2021-03-29 09:10:41 +0200

    Ajout Technoweb

--- Algorithmique/TDADS1120/src/main.c
+++ Algorithmique/TDADS1120/src/main.c
@@ -2,4 +2,16 @@
 
 int main(){
 
+//Création d'une nouvelle file
+
+
+//Affichage de la file
+for (int i = 0; i < ->last; i++);
+
+//Ajout d'un élément dans la file
+
+
+//Affichage de la file
+
+
 }
